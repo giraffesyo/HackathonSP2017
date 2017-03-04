2376390a4787d7df7db0e2c7240cceddf886bd13
Michael McQuade <mmcquad@mac20-cs.ms-2345.okstate.edu>
2017-03-04 06:08:44 -0600

    Batman
    This commit has no parents

---
+++ overwatch/Machine.java
@@ -0,0 +1,50 @@
+package overwatch;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.*;
+
+import java.net.URL;
+import java.net.URLConnection;
+
+class Machine {
+
+    private String rawStats;
+
+    Machine()
+    {
+        rawStats = "";
+    }
+
+    //updates raw stats and returns them
+    String updateStats(String userName, String userID ) {
+        try {
+            BufferedReader in;
+
+            URL url = new URL(constructURL(userName, userID));
+            URLConnection uc = url.openConnection();
+            uc.addRequestProperty("User-Agent",
+                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)");
+            HttpsURLConnection con = (HttpsURLConnection) uc;
+
+            in = new BufferedReader(new InputStreamReader(uc.getInputStream()));
+            System.out.println(in.readLine());
+
+        }
+        catch(IOException e){
+            //do something
+            e.printStackTrace();
+        }
+        return rawStats;
+    }
+
+    String getRawStats() {
+        return rawStats;
+    }
+
+    private String constructURL(String userName, String userID )
+    {
+        return "https://owapi.net/api/v3/u/" + userName + "-" + userID + "/stats";
+    }
+
+
+}
---
+++ overwatch/Main.java
@@ -0,0 +1,9 @@
+package overwatch;
+
+
+public class Main {
+    public static void main ( String[] args ){
+        Machine statsGrabber = new Machine();
+        System.out.println(statsGrabber.updateStats("giraffesyo","1652"));
+    }
+}
